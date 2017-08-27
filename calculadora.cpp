ce4a0d70b7617f727948b7d3a19546dabc98768f
MisterADV <31388715+MisterADV@users.noreply.github.com>
2017-08-27 20:19:49 +0200

    Add files via upload

---
+++ calculadora.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	float numero1,numero2,suma,resta,multiplicacion,division;
+	
+	cout<<"Digite un numero: ";
+	cin>>numero1;
+	
+	cout<<"Digite otro numero: ";
+	cin>>numero2;
+	
+	suma = numero1 + numero2;
+	resta = numero1 - numero2;
+	multiplicacion = numero1 * numero2;
+	division = numero1 / numero2;
+	
+	cout<<"\nLa suma da "<<suma<<endl;
+	cout<<"La resta da "<<resta<<endl;
+	cout<<"La multiplicacion da "<<multiplicacion<<endl;
+	cout<<"La division da "<<division<<endl;
+	
+	getch();
+}
